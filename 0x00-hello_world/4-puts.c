c8d2c346980517df26d22d198b789cff387e65d5
Genie ABASENGA SENKOBWA <genieabasenga@gmail.com>
2022-03-23 12:14:34 -0700

    modified puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
- *
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 
@@ -10,6 +9,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
- 
 
+}
