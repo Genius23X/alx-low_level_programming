3e340029165d6bc8b1571f3c0c2c1555803b1de0
Genie ABASENGA SENKOBWA <genieabasenga@gmail.com>
2022-03-29 15:39:09 -0700

    function that fills memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include 'main.h'
 
 /**
- *_memset - fills memory with a constant byte
+ * _memset - fills memory with a constant byte
  * @s: memory space to be filled
  * @b:bye to fill with
  * @n: number of spaces to fill
