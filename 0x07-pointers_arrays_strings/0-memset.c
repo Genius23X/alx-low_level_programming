253619f2afa4109fd4f678a05ef93670449f0ea0
Genie ABASENGA SENKOBWA <genieabasenga@gmail.com>
2022-03-29 15:57:29 -0700

    Modified memory filling

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 /**
  * _memset - fills memory with a constant byte
  * @s: memory space to be filled
- * @b:bye to fill with
+ * @b: bye to fill with
  * @n: number of spaces to fill
  *
  * Return: pointer to memory area s
