adb397ab2608878c9807d5166a01d9a05a2d066f
Genie ABASENGA SENKOBWA <genieabasenga@gmail.com>
2022-03-30 01:31:00 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,6 +7,7 @@
  *
  * Return: Always 0 (Success/correct)
  */
+
 int main(void)
 {
 int n;
@@ -16,17 +17,17 @@ n = rand() - RAND_MAX / 2;
 
 if (n == 0)
 {
-printf("%i is zero\n"; n);
+	printf("%i is zero\n", n);
 }
 
 else if (n < 0)
 {
-printf("%i is negative\n"; n);
+	printf("%i is negative\n", n);
 }
 
 else
 {
-printf("%i is positive\n"; n);
+	printf("%i is positive\n", n);
 }
 
 return (0)
